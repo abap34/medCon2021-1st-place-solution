5b73e1983ee10e4aa26d9813a05f2922799ef693
chizuchizu <ochimasumi@gmail.com>
2021-03-30 13:52:39 +0900

    Add seed everything

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,10 @@
+import os
+
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 import tqdm
+
 
 def read_data(data_contain_path):
     train = pd.read_csv(data_contain_path + '/train.csv')
@@ -18,3 +22,13 @@ def read_wave(paths):
         X.append(np.load(p))
     return np.stack(X)
 
+
+def seed_everything(seed=0):
+    physical_devices = tf.config.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
