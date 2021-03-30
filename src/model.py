c2653b4879f2a7ce16fe808ebdd5d7301705252c
chizuchizu <ochimasumi@gmail.com>
2021-03-30 20:12:06 +0900

    Add predict phase

--- src/model.py
+++ src/model.py
@@ -19,9 +19,10 @@ class BaseModel:
     def fit(self, train_x, train_y, val_x, val_y, fold_index):
         # 学習
         # --------------------------------------------------
+        checkpoint_filepath = './logs/{}/models/fold-{}.ckpt'.format(self.params.model_name, fold_index)
 
         model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-            filepath='./logs/{}/models/fold-{}.ckpt'.format(self.params.model_name, fold_index),
+            filepath=checkpoint_filepath,
             save_weights_only=True,
             monitor='val_auc',
             mode='max',
@@ -63,3 +64,11 @@ class BaseModel:
         # --------------------------------------------------
 
         return self.model.predict(val_x)
+
+    def predict(self, test_x, fold_index):
+        checkpoint_filepath = "./logs/{}/models/fold-{}.ckpt".format(self.params.model_name, fold_index)
+        self.model.load_weights(checkpoint_filepath)
+
+        pred_test = self.model.predict(test_x)[:, 0]
+
+        return pred_test
---
+++ src/predict.py
@@ -0,0 +1,79 @@
+from omegaconf import OmegaConf
+import sys
+
+sys.path.append("./models/")
+import utils
+
+# from wavenet import WaveNet
+# from resnet_1 import ResNet_1
+import wavenet
+import resnet_1
+import resnet_2
+import lstm
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import roc_auc_score
+
+import numpy as np
+
+from model import BaseModel
+
+MODEL_NAMES_DICT = {
+    'wavenet': wavenet.get_model,
+    "resnet_1": resnet_1.get_model,
+    "resnet_2": resnet_2.get_model,
+    "lstm": lstm.get_model
+}
+
+
+def main(param):
+    utils.seed_everything(0)
+    print('read csv...')
+    train, test, submit = utils.read_data("./data", pseudo=True)
+    print('read wave data...')
+    train_wave = utils.read_wave("./data/ecg/" + train["Id"] + ".npy")
+    test_wave = utils.read_wave("./data/ecg/" + test["Id"] + ".npy")
+    train_y = train["target"]
+
+    train["sex"] = train["sex"].replace({"male": 0, "female": 1})
+    test["sex"] = test["sex"].replace({"male": 0, "female": 1})
+
+    human_mask = train['label_type'] == 'human'
+
+    train_meta_human = train[human_mask][["sex", "age"]]
+    train_wave_human = train_wave[human_mask]
+
+    train_meta_auto = train[~human_mask][["sex", "age"]]
+    train_wave_auto = train_wave[~human_mask]
+
+    train_y_human = train_y[human_mask]
+    train_y_auto = train_y[~human_mask]
+
+    kf = StratifiedKFold(n_splits=5, random_state=10, shuffle=True)
+
+    test_preds = np.zeros(
+        [
+            5,
+            test_wave.shape[0]
+        ]
+    )
+
+    for (fold, (train_index, val_index)) in enumerate(kf.split(train_meta_human, train_y_human)):
+        print(f"{'=' * 20} fold {fold + 1} {'=' * 20}")
+
+        # foldごとに定義しないとリークしてしまう
+        model = BaseModel(
+            param,  # パラメータ
+            MODEL_NAMES_DICT[param.model_name]()  # コンパイル済みのモデル
+        )
+
+        test_preds[fold] = model.predict([test_wave, test[["sex", "age"]]], fold)
+
+    submit["target"] = test_preds.mean(axis=0)
+    submit.to_csv("./logs/{}/submission.csv".format(param.model_name), index=False)
+
+
+if __name__ == '__main__':
+    param = OmegaConf.from_cli()
+    print('params:', param)
+    main(param)
