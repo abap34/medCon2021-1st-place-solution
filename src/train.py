3d6c43e68c49787614c4d749e67ff93bfbc58929
Yuchi Yamaguchi <abap0002@gmail.com>
2021-04-02 01:38:14 +0900

    implement naive CV

--- src/train.py
+++ src/train.py
@@ -30,64 +30,91 @@ def main(param):
     train["sex"] = train["sex"].replace({"male": 0, "female": 1})
     test["sex"] = test["sex"].replace({"male": 0, "female": 1})
 
-    human_mask = train['label_type'] == 'human'
+    if param.validation == "custom":
+        human_mask = train['label_type'] == 'human'
 
-    train_meta_human = train[human_mask][["sex", "age"]]
-    train_wave_human = train_wave[human_mask]
+        train_meta_human = train[human_mask][["sex", "age"]]
+        train_wave_human = train_wave[human_mask]
 
-    train_meta_auto = train[~human_mask][["sex", "age"]]
-    train_wave_auto = train_wave[~human_mask]
+        train_meta_auto = train[~human_mask][["sex", "age"]]
+        train_wave_auto = train_wave[~human_mask]
 
-    train_y_human = train_y[human_mask]
-    train_y_auto = train_y[~human_mask]
+        train_y_human = train_y[human_mask]
+        train_y_auto = train_y[~human_mask]
 
-    kf = StratifiedKFold(n_splits=5, random_state=10, shuffle=True)
+        kf = StratifiedKFold(n_splits=5, random_state=10, shuffle=True)
 
-    val_preds = np.zeros(train_meta_human.shape[0])
+        val_preds = np.zeros(train_meta_human.shape[0])
 
-    utils.info('start training...')
+        utils.info('start training...')
 
-    for (fold, (train_index, val_index)) in enumerate(kf.split(train_meta_human, train_y_human)):
-        utils.info(f"{'=' * 20} fold {fold + 1} {'=' * 20}")
+        for (fold, (train_index, val_index)) in enumerate(kf.split(train_meta_human, train_y_human)):
+            utils.info(f"{'=' * 20} fold {fold + 1} {'=' * 20}")
 
-        # foldごとに定義しないとリークしてしまう
-        model = MODEL_NAMES_DICT[param.model_name](param)
+            # foldごとに定義しないとリークしてしまう
+            model = MODEL_NAMES_DICT[param.model_name](param)
 
-        train_input_wave = np.concatenate([
-            train_wave_human[train_index],
-            train_wave_auto
-        ])
+            train_input_wave = np.concatenate([
+                train_wave_human[train_index],
+                train_wave_auto
+            ])
 
-        train_input_meta = np.concatenate([
-            train_meta_human.iloc[train_index],
-            train_meta_auto
-        ])
+            train_input_meta = np.concatenate([
+                train_meta_human.iloc[train_index],
+                train_meta_auto
+            ])
 
-        train_y_concat = np.concatenate([
-            train_y_human.iloc[train_index],
-            train_y_auto
-        ])
+            train_y_concat = np.concatenate([
+                train_y_human.iloc[train_index],
+                train_y_auto
+            ])
 
-        val_input_wave = train_wave_human[val_index]
+            val_input_wave = train_wave_human[val_index]
 
-        val_input_meta = train_meta_human.iloc[val_index]
+            val_input_meta = train_meta_human.iloc[val_index]
 
-        val_y_concat = train_y_human.iloc[val_index]
+            val_y_concat = train_y_human.iloc[val_index]
 
-        val_pred = model.fit(
-            [train_input_wave, train_input_meta],
-            train_y_concat,
-            [val_input_wave, val_input_meta],
-            val_y_concat,
-            fold
-        )
+            val_pred = model.fit(
+                [train_input_wave, train_input_meta],
+                train_y_concat,
+                [val_input_wave, val_input_meta],
+                val_y_concat,
+                fold
+            )
 
-        # foldを忘れないよう注意. fitの帰り値はval_pred
+            # foldを忘れないよう注意. fitの帰り値はval_pred
 
-        val_preds[val_index] += val_pred
+            val_preds[val_index] += val_pred
 
-    utils.info("AUC score:", roc_auc_score(train_y[human_mask], val_preds))
-    pd.DataFrame(val_preds, columns=["pred"]).to_csv('./logs/{}/val_pred.csv'.format(param.model_name))
+        utils.info("AUC score:", roc_auc_score(train_y[human_mask], val_preds))
+        pd.DataFrame(val_preds, columns=["pred"]).to_csv('./logs/{}/val_pred_custom.csv'.format(param.model_name))
+
+    elif param.validation == "naive":
+
+        train_meta = train[["sex", "age"]]
+        kf = StratifiedKFold(n_splits=5, random_state=10, shuffle=True)
+        val_preds = np.zeros(train_meta.shape[0])
+        utils.info('start training...')
+        for (fold, (train_index, val_index)) in enumerate(kf.split(train_meta, train_y)):
+            utils.info(f"{'=' * 20} fold {fold + 1} {'=' * 20}")
+            model = MODEL_NAMES_DICT[param.model_name](param)
+            val_pred = model.fit(
+                [train_wave[train_index], train_meta.iloc[train_index]],
+                train_y[train_index],
+                [train_wave[val_index], train_meta.iloc[val_index]],
+                train_y[val_index],
+                fold
+            )
+
+            # foldを忘れないよう注意. fitの帰り値はval_pred
+
+            val_preds[val_index] += val_pred
+
+        utils.info("AUC score:", roc_auc_score(train_y, val_preds))
+        pd.DataFrame(val_preds, columns=["pred"]).to_csv('./logs/{}/val_pred_naive.csv'.format(param.model_name))
+    else:
+        raise ValueError
 
 
 if __name__ == '__main__':
