38b27af4b6937b707eb01aeb214078df048011da
chizuchizu <44621609+Chizuchizu@users.noreply.github.com>
2021-04-01 19:00:01 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 echo "train wavenet"
 python3 src/train.py model_name=wavenet epoch=25 batch_size=8
 echo "train resnet_1"
-python3 src/train.py model_name=resnet_1 epoch=25 batch_size=8
+python3 src/train.py model_name=resnet_1 epoch=25 batch_size=32
 echo "train resnet_2"
-python3 src/train.py model_name=resnet_2 epoch=25 batch_size=8
+python3 src/train.py model_name=resnet_2 epoch=25 batch_size=32
 echo "train lstm"
-python3 src/lstm.py model_name=lstm epoch=25 batch_size=8
+python3 src/train.py model_name=lstm epoch=25 batch_size=32
